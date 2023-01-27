8b48e503fdf6fc9b743b488094dc410009568bdf
ZahidulIslam1979 <Zahidoshin1979@gmail.com>
2023-01-26 22:41:23 -0500

    This is my first commit of this project.

---
+++ src/main/java/JavaCodingPracticePackage1_20/ReverseClass.java
@@ -0,0 +1,10 @@
+package JavaCodingPracticePackage1_20;
+
+public class ReverseClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/JavaCodingPracticePackage1_20/ScannerInputClass.java
@@ -0,0 +1,10 @@
+package JavaCodingPracticePackage1_20;
+
+public class ScannerInputClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
